8efd7876e0f3eac9cdb87bb297cc5e93230654dd
Ram Krishna Singh <rkscodes@gmail.com>
2022-09-02 14:34:02 +0530

    added api and html for basic site

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Meme-Website
+
 Creating a meme website using flask for fun
+
+## To run locally
+
+1. Clone this repo
+2. Change directory
+3. run -> `python meme_flask.py`
---
+++ meme_flask.py
@@ -0,0 +1,31 @@
+#!/opt/homebrew/opt/python/libexec/bin/python
+
+from webbrowser import get
+from flask import Flask, render_template
+import requests
+import json
+
+
+def get_meme():
+    # Uncomment these two lines and comment out the other url line if you want to use a specific meme subreddt
+    # sr = "/wholesomememes"
+    # url = "<https://meme-api.herokuapp.com/gimme>" + sr
+    url = "https://meme-api.herokuapp.com/gimme"
+    response = json.loads(requests.request("GET", url).text)
+    meme_large = response["preview"][-2]
+    subreddit = response["subreddit"]
+    return meme_large, subreddit
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    meme_pic, subreddit = get_meme()
+    return render_template("meme_index.html",
+                           meme_pic=meme_pic,
+                           subreddit=subreddit)
+
+
+app.run(host="0.0.0.0", port=80)
